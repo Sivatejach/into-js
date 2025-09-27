ad54291d2e2971f0ac4eeaeb009f93f3d76c38a8
Sivatejach <chsteja2003@gmail.com>
2025-09-27 11:40:04 +0530

    Cursor Commit

--- 01-js/easy/Anagram.js
+++ 01-js/easy/Anagram.js
@@ -5,15 +5,11 @@
 */
 
 function isAnagram(str1, str2) {
-    // Remove all non-alphanumeric characters and convert to lowercase
-    const cleanStr1 = str1.replace(/[^a-z0-9]/gi, '').toLowerCase();
-    const cleanStr2 = str2.replace(/[^a-z0-9]/gi, '').toLowerCase();
-    
-    // Sort the characters of both strings and compare
-    const sortedStr1 = cleanStr1.split('').sort().join('');
-    const sortedStr2 = cleanStr2.split('').sort().join('');
-    
-    return sortedStr1 == sortedStr2;
+  str1 = str1.toLowerCase().replace(/[^a-z0-9]/g, '');
+  str2 = str2.toLowerCase().replace(/[^a-z0-9]/g, '');
+  const sortedStr1 = str1.split('').sort().join('');
+  const sortedStr2 = str2.split('').sort().join('');  
+  return sortedStr1 == sortedStr2;
 
 }
 
