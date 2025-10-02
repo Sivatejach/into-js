33d9ac9c35a3646269acef4e9eb9639f898f206d
Sivatejach <chsteja2003@gmail.com>
2025-10-02 21:59:48 +0530

    hard problem added

--- week-2/01-async-js/hard(promise)/1-promisify-setTimeout.js
+++ week-2/01-async-js/hard(promise)/1-promisify-setTimeout.js
@@ -3,11 +3,11 @@
 */
 
 function wait(n) {
-    return new Promise((resolve) => {
+    return new Promise((resolve) =>{
         setTimeout(() => {
             resolve();
         }, n * 1000);
-    }); 
+    })
 }
 
 
